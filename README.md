c34451c9123064666c8349ceba64e76c529021df
farste <sfargali@gmail.com>
2018-11-09 13:41:09 -0500

    Stephen Fargali function returns items in current directory

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ type `make` in the `examples/` directory.) It should print `Testing: PASS`.
 
 Name at least three things that an operating system is responsible for handling?
 
+Manage computer's resources; hardware and devices,
+establish a user interface.
+provide services for app software
+
 ## Challenge 2
 
 Write a program in C, `lsls.c`, that prints out a directory listing for the
--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,12 +4,24 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv[])
 {
+  struct dirent *de;
+  DIR *dir = opendir("./");
+
+    if (dir == NULL) {
+      printf("Directory not found");
+      return 0;
+    }
+    while ((de = readdir(dir)) != NULL) {
+      printf("%s\n", de->d_name);
+    }
+
+    closedir(dir);
+
   // Parse command line
 
   // Open directory
-
   // Repeatly read and print entries
 
   // Close directory
